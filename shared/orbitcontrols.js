9dab3aaf481471d4c0f9069b1d6c849445b9485d
jhjuhn <jhjuhn.lok@gmail.com>
2017-03-21 19:09:54 +0000

    lorenz & roessler attractors

---
+++ shared/orbitcontrols.js
@@ -0,0 +1,180 @@
+function Orbitcontrols(camera, renderer, scene){
+	
+	this.camera = camera;
+	this.renderer = renderer;
+
+	this.dragstart = {};
+	this.dragstart.q2d = [0, 0];
+	this.dragstart.qspherical = {
+		o: [0, 0, 0, 1],
+		r: 2.5,
+		t: 0,
+		f: Math.PI/2
+	}
+
+	this.dollyrate = 0.1;
+
+	// records whether and how mouse events affect
+	// movement of camera.
+	this.state = {
+		rotation: false,
+		translation: false
+		};
+
+	// the position of the camera in spherical coords.
+	this.qspherical = {
+		o: [0, 0, 0, 1],	// origin
+		r: 2.5,			// radial distance
+		t: -Math.PI/2,		// azimuth
+		f: Math.PI/2		// elevation
+	};
+
+	// shortcuts for special views:
+	// { keycode: spherical coordinates }
+	this.views = {
+	97:	{ o: [0, 0, 0, 1], r: 2.5, t: 0, f: 0 },
+	99:	{ o: [0, 0, 0, 1], r: 2.5, t: Math.PI/2, f: 0 },
+	103:	{ o: [0, 0, 0, 1], r: 2.5, t: -Math.PI/2, f: Math.PI/2 },
+	};
+
+
+	this.attachhandlers(camera, renderer, scene);
+
+	return this;
+}
+// visual functions do nothing but are kept here for symmetry
+Orbitcontrols.prototype.visual_init = function(camera, renderer, scene){}
+Orbitcontrols.prototype.visual_sync = function(){}
+
+Orbitcontrols.prototype.qspherical_clamp = function(){
+	// clamp the coordinate values to prevent accidentally
+	// dividing by zero elsewhere.
+
+	while (this.qspherical.t >  2*Math.PI) { this.qspherical.t -= 2*Math.PI; }
+	while (this.qspherical.t < 0) { this.qspherical.t += 2*Math.PI; }
+
+	this.qspherical.f = Math.min(this.qspherical.f, Math.PI/2-1e-6);
+	this.qspherical.f = Math.max(this.qspherical.f, 1e-6-Math.PI/2);
+}
+
+Orbitcontrols.prototype.qspherical_fromcamera = function(){
+	var q = v4fromTHREEVector3(
+	this.camera.position).v4sub(this.qspherical.o);
+
+}
+
+Orbitcontrols.prototype.camera_fromqspherical = function(newsphq){
+
+	// if new coordinates are supplied, update the spherical
+	// coordinates to agree with the new ones. If this method
+	// is called w/o argument, then it just syncs the camera
+	// position with the stored spherical coords.
+	if (newsphq !== undefined) {
+		if (newsphq.o !== undefined) { this.qspherical.o = newsphq.o; }
+		if (newsphq.r !== undefined) { this.qspherical.r = newsphq.r; }
+		if (newsphq.t !== undefined) { this.qspherical.t = newsphq.t; }
+		if (newsphq.f !== undefined) { this.qspherical.f = newsphq.f; }
+	}
+	this.qspherical_clamp();
+
+	// calculate cartesian coordinates from spherical ones
+	// and assign the result to the camera position.
+	var newq = [
+		this.qspherical.r*Math.cos(this.qspherical.f)
+			*Math.cos(this.qspherical.t),
+		this.qspherical.r*Math.cos(this.qspherical.f)
+			*Math.sin(this.qspherical.t),
+		this.qspherical.r*Math.sin(this.qspherical.f),
+		1
+	].v4add(this.qspherical.o);
+	this.camera.position.fromArray(newq);
+
+	this.camera.lookAt(new THREE.Vector3().fromArray(this.qspherical.o));
+	
+}
+
+Orbitcontrols.prototype.keydownhandler = function(e){
+	if (this.views[e.which]) {
+		this.camera_fromqspherical(this.views[e.which]);
+	}
+}
+
+Orbitcontrols.prototype.mousedownhandler = function(e){
+	var q2d = [
+		e.clientX/e.target.clientWidth*2-1,
+		-e.clientY/e.target.clientHeight*2+1];
+
+	// if the camera is not already being moved, and the MMB is down,
+	// set the state such that mouse movement moves camera.
+	if ((e.which == 1) && !(this.state.translation || this.state.rotation)){
+		this.dragstart.q2d = q2d;
+		this.dragstart.qspherical = {
+			o: this.qspherical.o, r: this.qspherical.r,
+			t: this.qspherical.t, f: this.qspherical.f };
+
+		(e.shiftKey) ?
+		this.state.translation = true :
+		this.state.rotation = true;
+	}
+}
+
+Orbitcontrols.prototype.mousemovehandler = function(e){
+	// if currently moving the camera, calculate movement accordingly
+	var dq2d = [
+		(e.clientX/e.target.clientWidth*2-1) - this.dragstart.q2d[0],
+		(1-e.clientY/e.target.clientHeight*2) - this.dragstart.q2d[1]
+	];
+	var aspect = e.target.clientHeight/e.target.clientWidth;
+	if (this.state.rotation) {
+		this.qspherical.t = this.dragstart.qspherical.t - 4*dq2d[0];
+		this.qspherical.f = this.dragstart.qspherical.f - 4*aspect*dq2d[1];
+	}
+	if (this.state.translation) {
+		var dx = v4fromTHREEVector3(
+			(new THREE.Vector3(1, 0, 0)).applyEuler(
+			this.camera.rotation));
+		var dy = v4fromTHREEVector3(
+			(new THREE.Vector3(0, 1, 0)).applyEuler(
+			this.camera.rotation));
+		this.qspherical.o = v4add(this.dragstart.qspherical.o,
+		v4add(
+			v4mul(-this.qspherical.r*dq2d[0], dx),
+			v4mul(-this.qspherical.r*aspect*dq2d[1], dy))
+		);
+	}
+}
+
+Orbitcontrols.prototype.mouseuphandler = function(e){
+	var q2d = [
+		e.clientX/e.target.clientWidth*2-1,
+		-e.clientY/e.target.clientHeight*2+1];
+
+	// there's an assumption that the mouse can't be doing more than one
+	// thing at once (the mousedown handler doesn't trigger any additional
+	// statechanges once there is already an active state), so everytime
+	// a button is lifted it is safe to clear all states.
+	this.state.translation = false;
+	this.state.rotation = false;
+}
+
+Orbitcontrols.prototype.wheelhandler = function(e){
+	this.qspherical.r = Math.max(0, this.qspherical.r + e.deltaY*this.dollyrate);
+}
+
+Orbitcontrols.prototype.attachhandlers = function(camera, renderer, scene){
+	window.addEventListener("keydown", this.keydownhandler.bind(this));
+
+	renderer.domElement.addEventListener(
+			"mousedown", this.mousedownhandler.bind(this));
+	renderer.domElement.addEventListener(
+			"mousemove", this.mousemovehandler.bind(this));
+	renderer.domElement.addEventListener(
+			"mouseup", this.mouseuphandler.bind(this));
+	renderer.domElement.addEventListener(
+			"wheel", this.wheelhandler.bind(this));
+}
+
+Orbitcontrols.prototype.update = function(){
+	// wrapper for things to run every render
+	this.camera_fromqspherical();
+}
---
+++ shared/v4.js
@@ -0,0 +1,111 @@
+// adds functions to Array object for vector arithmetic.
+var V4_EPSILON = 1e-6;
+function v4fromTHREEVector3(v){
+	return [v.x, v.y, v.z, 1];
+}
+function v4clone(v){ return [v[0], v[1], v[2], 1]; }
+function v4add(u, v){ return [u[0]+v[0], u[1]+v[1], u[2]+v[2], 1]; }
+function v4sub(u, v){ return [u[0]-v[0], u[1]-v[1], u[2]-v[2], 1]; }
+function v4mul(a, v){ return [a*v[0], a*v[1], a*v[2], 1]; }
+function v4mag(v){ return Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]); }
+function v4mag2(v){ return v[0]*v[0]+v[1]*v[1]+v[2]*v[2]; }
+function v4epsilon(v) { return Math.abs(v4mag(v)) < V4_EPSILON; }
+function v4unit(v) {
+	if (v4epsilon(v)) { return [0, 0, 0, 1]; }
+	var m = 1/v4mag(v); return v4mul(m, v);
+}
+function v4dot(u, v){ return u[0]*v[0]+u[1]*v[1]+u[2]*v[2]; }
+function v4x(u, v){
+	return [u[1]*v[2]-u[2]*v[1],
+		u[2]*v[0]-u[0]*v[2],
+		u[0]*v[1]-u[1]*v[0],
+		1];
+}
+
+function v4ang(v, u, n){
+	// returns a result between + and - PI. If n is provided,
+	// angles are calculated CCW wrt n.
+	var x = v4x(u, v);
+	var ang = Math.atan2(v4mag(x), v4dot(u, v));
+	if (n && !v4epsilon(n)) {
+		return (v4dot(x, n) < 0 ? -ang : ang);
+	}
+	return ang;
+}
+
+function v4projontoplane(u, n){
+	//var normalcomponent = v4mul(v4dot(u, n), n);
+	return v4sub(u, v4mul(v4dot(u, n), n));
+}
+function v4projontovector(u, v){
+	return v4mul(v4dot(u, v)/v4mag2(v), v);
+}
+
+Array.prototype.v4fromTHREEVector3 = function(v){
+	this[0] = v.x; this[1] = v.y;
+	this[2] = v.z; this[3] = 1;
+	return this;
+}
+
+Array.prototype.v4clone = function(v){
+	return [this[0], this[1], this[2], 1];
+}
+Array.prototype.v4add = function(v){
+	this[0] += v[0]; this[1] += v[1];
+	this[2] += v[2]; this[3] = 1;
+	return this;
+}
+Array.prototype.v4sub = function(v){
+	this[0] -= v[0]; this[1] -= v[1];
+	this[2] -= v[2]; this[3] = 1;
+	return this;
+}
+Array.prototype.v4mul = function(a){
+	this[0] *= a; this[1] *= a;
+	this[2] *= a; this[3] = 1;
+	return this;
+}
+
+Array.prototype.v4mag = function(){
+	return Math.sqrt(this[0]*this[0]+this[1]*this[1]+this[2]*this[2]);
+}
+Array.prototype.v4mag2 = function(){
+	return this[0]*this[0]+this[1]*this[1]+this[2]*this[2];
+}
+Array.prototype.v4epsilon = function(){ return Math.abs(this.v4mag()) < V4_EPSILON; }
+Array.prototype.v4unit = function(){
+	if (this.v4epsilon()) { return [0, 0, 0, 1]; }
+	var m = 1/this.v4mag();
+	this[0] *= m; this[1] *= m;
+	this[2] *= m; this[3] = 1;
+	//console.log(this);
+	return this;
+}
+
+Array.prototype.v4dot = function(v){
+	return this[0]*v[0]+this[1]*v[1]*this[2]*v[2];
+}
+
+Array.prototype.v4x = function(v){
+	return [
+		this[1]*v[2]-this[2]*v[1],
+		this[2]*v[0]-this[0]*v[2],
+		this[0]*v[1]-this[1]*v[0],
+		1
+	];
+}
+
+Array.prototype.v4projontoplane = function(n){
+	this.v4sub(v4mul(v4dot(this, n), n));
+	return this;
+}
+
+Array.prototype.v4projontovector = function(v){
+	var newu = v4mul(v4dot(this, v)/v4mag2(v), v);
+	this[0] = newu[0];
+	this[1] = newu[1];
+	this[2] = newu[2];
+	this[3] = 1;
+	return this;
+}
+
